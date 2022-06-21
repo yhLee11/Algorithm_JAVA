01c05ab8176ea08a35f4fa1f42eb88d50d456732
yhLee11 <mrncd1@gmail.com>
2022-06-21 14:17:22 +0900

    prog_bfs

--- src/main/java/카카오프렌즈_컬러링북.java
+++ src/main/java/카카오프렌즈_컬러링북.java
@@ -1,41 +1,41 @@
 import java.util.*;
-class Point{
-    int x;
-    int y;
-    Point(int x, int y){
-        this.x=x;
-        this.y=y;
+
+class Solution {
+    static int[] dx={0,0,1,-1};
+    static int[] dy={1,-1,0,0};
+    static class Point{
+        int x;
+        int y;
+        Point(int x,int y){
+            this.x=x;
+            this.y=y;
+        }
     }
-}
-class 카카오프렌즈_컬러링북 {
-    int[] dx={1,-1,0,0};
-    int[] dy={0,0,1,-1};
-    boolean[][] visit;
-    public int[] solution(int m, int n, int[][] picture) {
+    public int[] 카카오프렌즈_컬러링북(int m, int n, int[][] picture) {
         int area = 0;
         int maxSize = 0;
-        visit=new boolean[m][n];
-        for(int i=0; i<m; i++){
-            for(int j=0;j<n;j++){
-                if (picture[i][j]!=0 && !visit[i][j]){
-                    maxSize=Math.max(bfs(m,n,picture,visit),maxSize);
+        boolean[][] visit=new boolean[m][n];
+        for (int i=0; i<m; i++){
+            for (int j=0;j<n;j++){
+                if (!visit[i][j] && picture[i][j]!=0){
+                    maxSize=Math.max(bfs(i,j,picture,visit),maxSize);
                     area++;
                 }
             }
         }
-        return new int[]{area,maxSize};
+        return new int[]{area, maxSize};
     }
-    public int bfs(int a, int b,int[][] picture, boolean[][] visit){
+    private static int bfs(int a, int b, int[][] picture, boolean[][] visit){
+        visit[a][b]=true;
         Queue<Point> q = new LinkedList<>();
         q.offer(new Point(a,b));
-        visit[a][b]=true;
         int cnt=1;
         while (!q.isEmpty()){
-            Point p = q.poll();
-            for(int i=0; i<4; i++){
+            Point p=q.poll();
+            for(int i=0;i<4;i++){
                 int nx=p.x+dx[i];
                 int ny=p.y+dy[i];
-                if (0<=nx&&nx<picture.length && 0<=ny&&ny<picture[0].length &&picture[nx][ny]==picture[a][b]&& !visit[nx][ny]){
+                if (0<=nx&&nx<picture.length && 0<=ny&&ny<picture[0].length && !visit[nx][ny] && picture[a][b]==picture[nx][ny]){
                     q.offer(new Point(nx,ny));
                     visit[nx][ny]=true;
                     cnt++;
